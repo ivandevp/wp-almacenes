5b9224494a9d1c3e53a7a921af23a135a5000831
ivandevp <medinadiazivan@gmail.com>
2017-01-18 07:07:47 -0500

    Submenu style

--- wp-content/themes/catalogo/footer.php
+++ wp-content/themes/catalogo/footer.php
@@ -7,24 +7,19 @@
             </div>
         </footer>
     </div>
-    
+
     <?php wp_footer(); ?>
-    
-	<script src="//code.jquery.com/jquery-2.2.4.min.js" type="text/javascript"></script>
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
+
+  	<script src="//code.jquery.com/jquery-2.2.4.min.js" type="text/javascript"></script>
+  	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js" type="text/javascript"></script>
-	<script type="text/javascript">
-	    $(document).ready(function() {
+  	<script type="text/javascript">
+        $(document).ready(function() {
             $("#modal-ads").modal('show');
             $('#carousel-marcas').carousel({
-        	    interval: 3000
-        	});
-        	$("li a").hover(function(){
-                $(".sub-menu").css("visibility", "visible");
-                }, function(){
-                $(".sub-menu").css("visibility", "hidden");
-            });
+          	    interval: 3000
+          	});
         });
-	</script>
+    </script>
 </body>
 </html>
